b3f5d96f7a30494b038954af26cdea7c50559f6a
Steve Phelps <79sphelps@gmail.com>
2024-05-03 12:36:04 -0700

    updating CORS header config

--- src/http-common.js
+++ src/http-common.js
@@ -6,5 +6,9 @@ export default axios.create({
   // baseURL: '',
   headers: {
     "Content-Type": "application/json",
-  },
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+    "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  }
 });
