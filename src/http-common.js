34347fe660fa7bf76b9f686d3223af5cf6e914fb
Steve Phelps <79sphelps@gmail.com>
2024-05-08 10:27:12 -0700

    updating baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:8080",
-  baseURL: "https://northone-fp6p0zqmv-79sphelps-projects.vercel.app/",
+  // baseURL: "https://northone-fp6p0zqmv-79sphelps-projects.vercel.app/",
+  baseURL: "https://northone-79sphelps-projects.vercel.app/",
   // baseURL: '',
   headers: {
     "Content-Type": "application/json",
