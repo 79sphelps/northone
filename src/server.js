001492756ab34a3638f2d95f907b84c942388ba9
Steve Phelps <79sphelps@gmail.com>
2024-05-09 05:46:14 -0700

    updating the baseURL

--- src/server.js
+++ src/server.js
@@ -10,7 +10,8 @@ if (process.env.NODE_ENV !== "dev") {
 } else {
   let corsOptions = {
       // origin: 'http://localhost:3000'
-      origin: 'https://northone-fp6p0zqmv-79sphelps-projects.vercel.app/'
+      // origin: 'https://northone-fp6p0zqmv-79sphelps-projects.vercel.app/'
+      origin: 'northone.vercel.app',
   };
   app.use(cors(corsOptions));
 }
