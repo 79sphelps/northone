a598d19cf375ddaea20f76fe1f41c26e2414e8d7
Steve Phelps <79sphelps@gmail.com>
2026-01-17 13:24:08 -0800

    Adding checkbox status updates to event list

--- src/components/CalendarList.tsx
+++ src/components/CalendarList.tsx
@@ -1,7 +1,12 @@
 import React, { memo, useState } from "react";
 // import { useCalendarList } from "./useCalendarList.ts";
 import { useAppSelector } from "../redux/selectors";
-import { ICalendarEvent } from "../redux/actions/index.ts";
+import { useAppDispatch } from "../redux/store/index.ts";
+import {
+  getCalendarEvents,
+  ICalendarEvent,
+  updateCalendarEvent,
+} from "../redux/actions/index.ts";
 import { selectIsLoading } from "../redux/selectors/index.ts";
 import Spinner from "react-bootstrap/Spinner";
 
@@ -9,7 +14,7 @@ interface ICalendarListProps {
   calendarEvents: ICalendarEvent[];
   setActiveCalendarEvent: (
     calendarEvent: ICalendarEvent,
-    index: number
+    index: number,
   ) => void;
   currentIndex: number;
 }
@@ -19,50 +24,80 @@ const CalendarList: React.FC<ICalendarListProps> = memo(
     // const { removeAllCalendarEvents }: { removeAllCalendarEvents: () => void } =
     //   useCalendarList();
     const isLoading: boolean = useAppSelector(selectIsLoading);
-    const [filter, setFilter] = useState('all');
+    const [filter, setFilter] = useState("all");
+    const dispatch = useAppDispatch();
 
     if (isLoading) {
       return (
-        <div className="is-loading" data-testid='calendar-list-events-loading-id'>
-          <Spinner animation="border" style={{ height: "200px" }}/>
-          <p className="loading-text">
-            Loading Calendar Data...
-          </p>
+        <div
+          className="is-loading"
+          data-testid="calendar-list-events-loading-id"
+        >
+          <Spinner animation="border" style={{ height: "200px" }} />
+          <p className="loading-text">Loading Calendar Data...</p>
         </div>
       );
     }
 
     const handleFilter = (e: React.ChangeEvent<HTMLSelectElement>) => {
       setFilter(e.target.value);
-    }
+    };
+
+    const updateEventStatus = (item: ICalendarEvent) => {
+      const i = calendarEvents.find((v) => v._id === item._id);
+      if (i) {
+        const n = { ...i, status: !item.status };
+        dispatch(updateCalendarEvent({ _id: n._id, ...n }));
+        // dispatch(getCalendarEvent({ _id: n._id }))
+        setTimeout(() => dispatch(getCalendarEvents()), 500);
+      }
+    };
 
     return (
-      <div className='calendar-events' data-testid="calendar-list-id">
-
-        <div className='calendar-list-header-and-filter'>
+      <div className="calendar-events" data-testid="calendar-list-id">
+        <div className="calendar-list-header-and-filter">
           <h4 data-testid="calendar-list-header-id">Calendar Events</h4>
           <select onChange={handleFilter}>
-            <option value='all'>All</option>
-            <option value='pending'>Not Started</option>
-            <option value='completed'>Done</option>
+            <option value="all">All</option>
+            <option value="pending">Not Started</option>
+            <option value="completed">Done</option>
           </select>
         </div>
 
-        <ul className="calendar-list list-group" aria-labelledby="calendar-events-heading" aria-label="calendar events">
+        <ul
+          className="calendar-list list-group"
+          aria-labelledby="calendar-events-heading"
+          aria-label="calendar events"
+        >
           {calendarEvents &&
             calendarEvents
-              .filter(v => filter === 'pending' ? !v.status : filter === 'completed' ? v.status : v)
+              .filter((v) =>
+                filter === "pending"
+                  ? !v.status
+                  : filter === "completed"
+                    ? v.status
+                    : v,
+              )
               .map((calendarEvent, index) => (
-              <li
-                className={
-                  "list-item " + `${calendarEvent.status ? 'text-decoration-line-through' : 'none'}`
-                }
-                onClick={() => setActiveCalendarEvent(calendarEvent, index)}
-                key={calendarEvent._id}
-              >
-                {calendarEvent.title}
-              </li>
-            ))}
+                <div className="event-item" key={calendarEvent._id}>
+                  <li
+                    className={
+                      "list-item " +
+                      `${calendarEvent.status ? "text-decoration-line-through" : "none"}`
+                    }
+                    onClick={() => setActiveCalendarEvent(calendarEvent, index)}
+                    key={calendarEvent._id}
+                  >
+                    {calendarEvent.title}
+                  </li>
+                  <input
+                    className="event-item-checkbox"
+                    type="checkbox"
+                    onChange={() => updateEventStatus(calendarEvent)}
+                    checked={calendarEvent.status ? true : false}
+                  />
+                </div>
+              ))}
         </ul>
       </div>
 
@@ -99,7 +134,7 @@ const CalendarList: React.FC<ICalendarListProps> = memo(
       //   </button>
       // </div>
     );
-  }
+  },
 );
 
 export default CalendarList;
